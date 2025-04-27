603e1457d07b72e7e3d0e6616a2a705611f48697
Fadlan <fadlank.noer@gmail.com>
2025-04-27 13:18:53 +0700

    chore: add integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,23 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import alpinejs from "@astrojs/alpinejs";
+
+import preact from "@astrojs/preact";
+
+import react from "@astrojs/react";
+
+import solidJs from "@astrojs/solid-js";
+
+import svelte from "@astrojs/svelte";
+
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-	    plugins: [tailwindcss()],
-	},
+  vite: {
+        plugins: [tailwindcss()],
+    },
+
+  integrations: [alpinejs(), preact(), react(), solidJs(), svelte(), vue()],
 });
