5656e8a6a5bb6c6f0d3158dd9cc418220377c068
Fadlan Noer <fadlan.kautsar@meteor.id>
2025-05-01 19:44:26 +0700

    feat(wip): hero component svg consume

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ export default defineConfig({
     }),
     solidJs({
       devtools: true,
-      include: ['**/solid/*'],
+      include: ['**/solid/*', '**/solid/svgs/*'],
     })
   ],
 });
--- src/components/solid/HeroLayer.tsx
+++ src/components/solid/HeroLayer.tsx
@@ -1,28 +1,45 @@
+import "../../../styles/HeroLayer.css";
+import DiagonalArrow from "./svgs/DiagonalArrow";
+
 const HeroLayer = () => {
 
   return (
-    <div class="hero-container w-screen h-screen bg-(--primary-color) text-(--text-color-one)">
-      <div class="flex-container w-full h-full flex">
+    <div class="hero-container w-screen h-screen bg-(--primary-color) text-(--text-color-one) flex items-center justify-center">
+      <div class="flex-container w-5xl h-6/10 flex justify-between">
 
         {/* Left Side Hero */}
-        <div>
-          <p class="text-5xl font-bold">MY NAME IS FADLAN KAUTSAR NOER</p>
-          <p>Software Engineer based on Indonesia</p>
-          <button>Know Me More</button>
-          <div class="hero-contact flex">
+        <div class="left-side-text-container basis-xs my-auto">
+
+          <p class="text-6xl text-justify">MY NAME IS <b>FADLAN NOER...</b></p>
+
+          <br/>
+
+          <p><span class="font-bold italic">Software Engineer</span> based in <span class="font-bold italic">Indonesia</span></p>
+
+          <div class="button-cv flex items-center justify-center my-[48px] w-[258px] h-[56px] bg-white text-(--primary-color)">
+            <p class="mr-3">Know me by my CV!</p>
+
+            <div class="w-6 h-6 flex justify-center items-center bg-linear-to-tr from-[#44749d] via-[#44749d] to-(--text-color-one)">
+              <DiagonalArrow className="w-4" fill="var(--text-color-one)"/>
+            </div>
+          </div>
+
+          <div class="hero-contact flex text-xs">
             <p>+62 895-3528-37926</p>
             <p>fadlank.noer@gmail.com</p>
           {/* Close hero-contact */}
           </div>
+
         </div>
 
         {/* Right Side Hero */}
-        <div class="img-container w-50">
-          <img src="../../../public/assets/half-body.png"/>
+        <div class="img-container">
+          <img src="/assets/half-body.png" class="w-70"/>
         </div>
 
         {/* Socmed Floating */}
-        <div>
+        <div class="rotated-socmed shrink-2">
+          <p>parameter</p>
         </div>
 
       {/* Close flex-container */}
