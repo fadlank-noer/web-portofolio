7468544c54cbf7f61c27c8bb8d1abcdccb2b6622
Fadlan <fadlank.noer@gmail.com>
2025-09-08 07:38:01 +0700

    fix: rounded icons phone & email

--- src/components/solid/ui/ContactMe.tsx
+++ src/components/solid/ui/ContactMe.tsx
@@ -1,39 +1,24 @@
 /** @jsxImportSource solid-js */
 
 import Assets from "./Assets";
+import RoundedIcon from "./RoundedIcon";
 
 const ContactMe = () => {
     return (
-        <div style={{"font-size": "1.5rem"}} class="hero-contact w-[48%] mx-auto flex flex-col lg:flex-row text-sm justify-between">
+        <div style={{"font-size": "1.5rem"}} class="hero-contact w-[50%] mx-auto flex flex-col lg:flex-row text-sm justify-between">
 
             <div class="phone-container flex-1 flex my-5 items-center">
-              <div class="w-6 h-6 bg-(--text-color-two) flex items-center justify-center mr-3 rounded-xl">
-                <Assets 
-                  assetName="phone"
-                  attribute={() => {
-                    return {
-                      class: "w-4",
-                      fill: "#000000"
-                    } 
-                  }} 
-                />
+              <div class="w-15 bg-(--text-color-two) flex items-center justify-center mr-3 rounded-full">
+                <RoundedIcon assetName="phone" class="w-4 h-4"/>
               </div>
-              <a class="hover:underline" href="https://api.whatsapp.com/send?phone=62895352837926&text=Hai%20Dlan%2C%20Aku%20datang%20dari%20Website-Mu!">Contact me By Whatsapp!</a>
+              <a class="hover:underline ml-2" href="https://api.whatsapp.com/send?phone=62895352837926&text=Hai%20Dlan%2C%20Aku%20datang%20dari%20Website-Mu!">Contact me By Whatsapp!</a>
             </div>
 
             <div class="email-container flex-1 flex items-center">
-              <div class="w-6 h-6 bg-(--text-color-two) flex items-center justify-center mr-3 rounded-xl">
-                <Assets
-                  assetName="email"
-                  attribute={() => {
-                    return {
-                      class: "w-4",
-                      fill: "#000000"
-                    } 
-                  }} 
-                />
+              <div class="w-15 bg-(--text-color-two) flex items-center justify-center mr-3 rounded-full">
+                <RoundedIcon assetName="email" class="w-4 h-4"/>
               </div>
-              <a class="hover:underline" href="mailto:fadlank.noer@gmail.com">Contact me By Email!</a>
+              <a class="hover:underline ml-2" href="mailto:fadlank.noer@gmail.com">Contact me By Email!</a>
             </div>
 
           {/* Close hero-contact */}
